b08dd410e4c633510007a1ce813a911db0bb90e8
Владислав Левшин <levshin.va18@physics.msu.ru>
2022-09-05 14:27:25 +0300

    Добавьте файлы проекта.

---
+++ Обработочка/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace Обработочка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.hScrollBar1);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(791, 571);
+            this.splitContainer1.SplitterDistance = 635;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(635, 571);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 171);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button4);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button5);
+            this.splitContainer2.Size = new System.Drawing.Size(152, 62);
+            this.splitContainer2.SplitterDistance = 72;
+            this.splitContainer2.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 62);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "PREVIOUS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 62);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "NEXT";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "COLOR";
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button3.Location = new System.Drawing.Point(0, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "MOD";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 282);
+            this.hScrollBar1.Maximum = 1000;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(152, 20);
+            this.hScrollBar1.TabIndex = 4;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 302);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(152, 60);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(0, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 61);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 423);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(152, 75);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(0, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(791, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.openToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem
+            // 
+            this.тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem.Name = "тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem";
+            this.тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem.Size = new System.Drawing.Size(227, 22);
+            this.тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem.Text = "ТЕБЕ НИКТО НЕ ПОМОЖЕТ";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(791, 595);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem тЕБЕНИКТОНЕПОМОЖЕТToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+    }
+}
+
---
+++ Обработочка/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Обработочка
+{
+    public partial class Form1 : Form
+    {
+        private List<byte[,,]> cifra1;
+        private List<byte[,,]> cifra2;
+        private List<double> proc;
+        private byte[,] kol;
+        private bool q = true;
+        private Color MainColor;
+        private bool mod = true;
+        public int x;
+        public int y;
+        public int colz;
+        public int mesto;
+        public int k;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Processing(int i)
+        {
+            if (i % 3 == 0)
+            {
+                int ramka = hScrollBar1.Value;
+                int y = i / 3 / cifra1[k].GetLength(2);
+                int x = i / 3 % cifra1[k].GetLength(2);
+                if (Math.Pow(MainColor.R - cifra1[k][0, y, x], 2) + Math.Pow(MainColor.G - cifra1[k][1, y, x], 2) + Math.Pow(MainColor.B - cifra1[k][2, y, x], 2) < ramka * ramka)
+                {
+                    kol[y, x] = 1;
+                    cifra2[k][0, y, x] = (byte)(255 - MainColor.R);
+                    cifra2[k][1, y, x] = (byte)(255 - MainColor.G);
+                    cifra2[k][2, y, x] = (byte)(255 - MainColor.B);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cifra1.Count; i++)
+            {
+                cifra2[i] = (byte[,,])cifra1[i].Clone();
+                proc[i] = 0;
+            }
+            button2.Text = "ORIGINAL";
+            if (mod)
+            {
+                k = 0;
+                while (k<cifra1.Count)
+                {
+                    kol = new byte[cifra1[k].GetLength(1), cifra1[k].GetLength(2)];
+                    Parallel.For(0, cifra1[k].GetLength(1) *cifra1[k].GetLength(2) * 3, Processing);
+                    for (int i=0; i<cifra1[k].GetLength(1); i++)
+                    {
+                        for (int j = 0; j < cifra1[k].GetLength(2); j++)
+                        {
+                            if (kol[i, j] == 1)
+                            {
+                                proc[k] += 1;
+                            }
+                        }
+                    }
+                    proc[k] *= (double)100/(cifra1[k].GetLength(1) * cifra1[k].GetLength(2));
+                    k += 1;
+                }
+                button2.Enabled = true;
+                textBox1.Text = proc[mesto].ToString();
+            }
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            MainColor = colorDialog1.Color;
+            pictureBox2.BackColor = MainColor;
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            label1.Text = hScrollBar1.Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (mod == true)
+            {
+                mod = false;
+                button3.Text = "Contrast changing";
+            }
+            else
+            {
+                mod = true;
+                button3.Text = "Pixels counting";
+            }
+        }
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = true;
+            cifra1 = new List<byte[,,]>();
+            cifra2 = new List<byte[,,]>();
+            mesto = 0;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = false;
+                string[] files = openFileDialog1.FileNames;
+                proc = new List<double>();
+                foreach (string f in files)
+                {
+                    cifra1.Add(BitmapToRgb(new Bitmap(@f)));
+                    cifra2.Add(BitmapToRgb(new Bitmap(@f)));
+                    proc.Add(0);
+                }
+                pictureBox1.Image = RgbToBmp(cifra1[mesto]);
+                button1.Enabled = true;
+                if (cifra1.Count == 1)
+                {
+                    button5.Enabled = false;
+                }
+            }
+        }
+        public unsafe static byte[,,] BitmapToRgb(Bitmap bmp)
+        {
+            int width = bmp.Width,
+                height = bmp.Height;
+            byte[,,] res = new byte[3, height, width];
+            BitmapData bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            try
+            {
+                byte* curpos;
+                fixed (byte* _res = res)
+                {
+                    byte* _r = _res, _g = _res + width * height, _b = _res + 2 * width * height;
+                    for (int h = 0; h < height; h++)
+                    {
+                        curpos = ((byte*)bd.Scan0) + h * bd.Stride;
+                        for (int w = 0; w < width; w++)
+                        {
+                            *_b = *(curpos++); ++_b;
+                            *_g = *(curpos++); ++_g;
+                            *_r = *(curpos++); ++_r;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return res;
+        }
+        public unsafe static Bitmap RgbToBmp(byte[,,] rgb)
+        {
+            Bitmap bmp = new Bitmap(rgb.GetLength(2),rgb.GetLength(1));
+            BitmapData bd = bmp.LockBits(new Rectangle(0, 0, rgb.GetLength(2), rgb.GetLength(1)), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            try
+            {
+                byte* curpos = (byte*)bd.Scan0;
+                for (int h = 0; h < rgb.GetLength(1); h++)
+                {
+                    for (int w = 0; w < rgb.GetLength(2); w++)
+                    {
+                        curpos[2] = rgb[0, h, w];
+                        curpos[1] = rgb[1, h, w];
+                        curpos[0] = rgb[2, h, w];
+                        curpos += 3;
+                    }
+
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+            }
+            return bmp;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            using (Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height))
+            {
+                pictureBox1.DrawToBitmap(bmp, pictureBox1.ClientRectangle);
+                MainColor= bmp.GetPixel(e.X, e.Y);
+                pictureBox2.BackColor = MainColor;
+                colorDialog1.Color=MainColor;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (mesto + 1 < cifra1.Count)
+            {
+                pictureBox1.Image = RgbToBmp(cifra1[mesto + 1]);
+                button2.Text = "ORIGINAL";
+                mesto += 1;
+                button4.Enabled = true;
+                textBox1.Text = proc[mesto].ToString();
+            }
+            if (mesto  == cifra1.Count - 1)
+            {
+                button5.Enabled=false;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (mesto - 1 >= 0)
+            {
+                pictureBox1.Image = RgbToBmp(cifra1[mesto - 1]);
+                button2.Text = "ORIGINAL";
+                mesto -= 1;
+                button5.Enabled = true;
+               // textBox1.Text = proc[mesto].ToString();
+            }
+            if (mesto  == 0)
+            {
+                button4.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (q)
+            {
+                pictureBox1.Image = RgbToBmp(cifra2[mesto]);
+                button2.Text = "CHANGED";
+                q = false;
+            }
+            else
+            {
+                pictureBox1.Image = RgbToBmp(cifra1[mesto]);
+                button2.Text = "ORIGINAL";
+                q = true;
+            }
+
+        }
+    }
+}
